4d97433e2e94b3c7c6088d76ff51117e54c30345
LeakyEarth2000 <yashbatish123@icloud.com>
2025-01-31 16:28:31 +1100

    v1.1
    Added ASMRProg login page, but shit aint working
    
    Login page, bcoz need to create two account, teacher and student

--- views.py
+++ views.py
@@ -5,3 +5,7 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template('index.html')
+
+@views.route("/login")
+def login():
+    return render_template('login.html')
