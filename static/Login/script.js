49072df0060bb5e7c97ec2749b75f2eae013aa45
LeakyEarth2000 <yashbatish123@icloud.com>
2025-01-31 20:12:53 +1100

    v1.14
    Added NHE colour scheme on authentication page

--- static/Login/script.js
+++ static/Login/script.js
@@ -5,7 +5,7 @@ const registerForm = document.querySelector(".register-form");
 
 
 loginBtn.addEventListener('click', () => {
-    loginBtn.style.backgroundColor = "#21264D";
+    loginBtn.style.backgroundColor = "#09212b";
     registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
 
     loginForm.style.left = "50%";
@@ -19,7 +19,7 @@ loginBtn.addEventListener('click', () => {
 
 registerBtn.addEventListener('click', () => {
     loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-    registerBtn.style.backgroundColor = "#21264D";
+    registerBtn.style.backgroundColor = "#09212b";
 
     loginForm.style.left = "150%";
     registerForm.style.left = "50%";
